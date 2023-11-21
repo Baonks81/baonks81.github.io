fdeb05863b56bf1da9540ff88eeef84321b060f2
Baonks81 <69669474+Baonks81@users.noreply.github.com>
2023-11-21 19:30:16 +0700

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -61,18 +61,3 @@ window.addEventListener("load", () => {
       }
   });
 });
-
-const blurDivs = document.querySelectorAll(".blur-load");
-blurDivs.forEach(div => {
-    const img = div.querySelector("img");
-
-    function loaded () {
-        div.classList.add("loaded");
-    }
-
-    if (img.complete) {
-        loaded();
-    } else {
-        img.addEventListener("load", loaded);
-    }
-});
